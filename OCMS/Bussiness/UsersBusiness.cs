564df19d0b91bbbe1eba0bdf49a399b0e6758b0f
tynGuardian <37328392+tynGuardian@users.noreply.github.com>
2018-03-16 18:33:07 +0800

    business

---
+++ OCMS/Bussiness/UsersBusiness.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OCMS.MODEL;
+using OCMS.DAL;
+
+namespace OCMS.Bussiness
+{
+    public class UsersBusiness
+    {
+        UsersDataAccess usersdata = new UsersDataAccess();
+        UsersDataAccess validateuser = new UsersDataAccess();
+        public bool SaveUsersInfo(UsersModel usersmodel)
+        {
+            try
+            {
+                if (usersdata.SaveUsersInformation(usersmodel) == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+           catch(Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public bool validateUserInfo(UsersModel usermodel)
+        {
+            try
+            {
+                if (validateuser.validateUserInformation(usermodel) == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
