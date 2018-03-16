fb3f4998170ea73e0c18d9a7974740d5b704df2b
tynGuardian <37328392+tynGuardian@users.noreply.github.com>
2018-03-16 18:32:01 +0800

    Add files via upload

---
+++ OCMS/DAL/UsersDataAccess.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using OCMS.MODEL;
+using System.Data;
+using OCMS.Class;
+
+namespace OCMS.DAL
+{
+    public class UsersDataAccess
+    {
+        public bool SaveUsersInformation(UsersModel UsersModel)
+        {
+            try
+            {
+                int result;
+                DBResource newConnection = new DBResource();
+                newConnection.DBOpen();
+
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = DBResource.conn;
+                comm.CommandText = "dbo.SaveUserInformation";
+                comm.CommandType = CommandType.StoredProcedure;
+
+                comm.Parameters.AddWithValue("@LastName", UsersModel.lastname);
+                comm.Parameters.AddWithValue("@FirstName", UsersModel.firstname);
+                comm.Parameters.AddWithValue("@MiddleName", UsersModel.middlename);
+                comm.Parameters.AddWithValue("@UserCode", UsersModel.usercode);
+                comm.Parameters.AddWithValue("@UserPassword", UsersModel.userpass);
+                comm.Parameters.AddWithValue("@Role", UsersModel.role);
+                comm.Parameters.AddWithValue("@LicenseNo", UsersModel.licenseno);
+                comm.Parameters.AddWithValue("@active", UsersModel.active);
+                result = comm.ExecuteNonQuery();
+                newConnection.DBClose();
+                if (result > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool validateUserInformation(UsersModel UsersModel)
+        {
+
+            DBResource newConnection = new DBResource();
+
+            using (SqlConnection myConnection = new SqlConnection(newConnection.connectionString.ToString()))
+            {
+
+                string query = "dbo.CheckUserAccount '" + UsersModel.usercode + "','" + UsersModel.userpass + "'";
+                SqlCommand cmd = new SqlCommand(query, myConnection);
+                myConnection.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            clsGlobal.lblrole = dr["role"].ToString().Trim();
+                            return true;
+                        }
+                    }
+                    myConnection.Close();
+                    return false;
+                }
+            }
+
+        }
+
+    }
+}
