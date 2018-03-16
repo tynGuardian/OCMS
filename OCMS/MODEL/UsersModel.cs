709a4b3808eabb1ddf94db3f9af3cb5a27ea8cf6
tynGuardian <37328392+tynGuardian@users.noreply.github.com>
2018-03-16 18:31:07 +0800

    Add files via upload

---
+++ OCMS/MODEL/UsersModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace OCMS.MODEL
+{
+    public class UsersModel
+    {
+        public string lastname { get; set; }
+        public string firstname { get; set; }
+        public string middlename { get; set; }
+        public string usercode { get; set; }
+        public string userpass { get; set; }
+        public string role { get; set; }
+        public string licenseno { get; set; }
+        public int active { get; set; }
+
+    }
+}
